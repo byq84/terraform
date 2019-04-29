dec0349d9b36aa7d7e6ab2818f6e96bc2d302f77
byq84 <50098016+byq84@users.noreply.github.com>
2019-04-29 11:02:58 +0200

    main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Configure the AWS Provider
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
+# Create an EC2 instance
+resource "aws_instance" "ec2-terraform" {
+  ami           = "ami-034b77cdfa2cdf3fd"
+  instance_type = "t2.micro"
+  security_groups = [ "launch-wizard-1" ]
+tags {
+    Name = "Instance of EC2 Created by Terraform"
+  }
+
+user_data = <<-EOF
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install httpd -y
+            sudo service httpd start
+            sudo chkconfig httpd on
+            echo "My New EC2 Instance Created by Terraform" >> /var/www/html/index.html
+            EOF
+
+}
